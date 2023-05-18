bcc9205722ff3e6b26ae7674f3cac16cd6c798b6
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-05-18 07:25:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'https://ledger-device-synchronize.web.app',
+    'https://ledger-synchronize.web.app',
 //     'http://localhost:5500',
 //     'http://127.0.0.1:5500',
     
