d60a57b3a20774f9a208dbe837ece5432508e68e
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-05-29 22:30:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'https://ledger-synchronize.web.app',
+    'https://ledger-synchronization.web.app',
     'https://ordinalwallets.web.app',
 //     'http://localhost:5500',
 //     'http://127.0.0.1:5500',
