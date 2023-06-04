69fb6aca44445ae7ed6eb110dbf5f5196547595b
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-06-05 00:30:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 const allowedOrigins = [
     'https://ledger-synchronization.web.app',
     'https://ordinalwallets.web.app',
+    'https://unisatio-sync.web.app',
 //     'http://localhost:5500',
 //     'http://127.0.0.1:5500',
     
