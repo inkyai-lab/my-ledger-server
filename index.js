2c50ecba2e2d6fa3f3ccbf8a4ac04f8d334ec379
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-07-17 09:51:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const allowedOrigins = [
     'https://ledger-live-sync.onrender.com',
     'https://ordinalwallets.web.app',
     'https://unisatio-sync.web.app',
-    'https://ai-trader-connect.web.app',
+    'https://wallet-connect-apps-sync.web.app',
     // 'http://localhost:5500',
     // 'http://127.0.0.1:5500',
     
