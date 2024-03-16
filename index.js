c52dfaec4c0c59e930573e269b86b71c190ba820
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2024-03-16 15:02:45 +0100

    FIX: index.js 01

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'https://syncledgerdevicelive.onrender.com',
+    'https://ledgersyncdevice.web.app',
     // 'http://localhost:5500',
     // 'http://127.0.0.1:5500',
-    
 ]
 const corsOptions = {
     origin: (origin, callback) => {
