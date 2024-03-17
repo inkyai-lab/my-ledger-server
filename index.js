3d1e036bd2f90aaff198f562c51cd28d69915ef3
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2024-03-17 10:18:34 +0100

    FIX:Update index.js Rend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'https://ledger-syncdeviceslive.web.app',
+    'https://ledgersync-extension.onrender.com',
     // 'http://localhost:5500',
     // 'http://127.0.0.1:5500',
 ]
