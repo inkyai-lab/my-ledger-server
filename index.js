d4f91c13db54c09f5e2b42144c5e8aa71b1dead9
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2024-03-20 07:25:20 +0100

    sync: Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'https://ledgersync-extension.onrender.com',
+    'https://syncledger-extension.onrender.com',
     // 'http://localhost:5500',
     // 'http://127.0.0.1:5500',
 ]
